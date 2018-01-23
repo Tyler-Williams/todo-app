d2cb82d701e74697690c3c5cfcd63bde308eb7a0
Tyler-Williams <github@tylerclarkwilliams.com>
2018-01-23 09:57:17 -0800

    finished tutorial

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done}).length}} </p>
+    <p>Pending Tasks: {{todos.filter(todo => {return !todo.done}).length}} </p>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :key="todo.id" v-bind:todo="todo"/>
+  </div>
+</template>
+
+<script>
+import Todo from './Todo'
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos.splice(todoIndex, 1)
+    },
+    completeTodo (todo) {
+      const todoIndex = this.todos.indexOf(todo)
+      this.todos[todoIndex].done = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
